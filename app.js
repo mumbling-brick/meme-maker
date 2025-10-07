1b2176adc8458920d1d350e3f5420b39365b294a
mumbling-brick <123447936+mumbling-brick@users.noreply.github.com>
2025-10-07 22:11:59 +0900

    Drawing house

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ canvas.height = 800;
 
 const ctx = canvas.getContext("2d");
 
-ctx.moveTo(100, 100);
-ctx.lineTo(200, 100);
-ctx.lineTo(200, 200);
-ctx.lineTo(100, 200);
-ctx.lineTo(100, 100);
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.lineWidth = 2;
+ctx.strokeRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
 
+ctx.beginPath();
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
 ctx.fillStyle = "red";
 ctx.fill();
