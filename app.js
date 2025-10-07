1a3964ac68a7cb348fb98d884473d378aeb8d727
mumbling-brick <123447936+mumbling-brick@users.noreply.github.com>
2025-10-07 22:21:39 +0900

    Drawing person

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ canvas.height = 800;
 
 const ctx = canvas.getContext("2d");
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.strokeRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
+ctx.fillRect(200 - 40, 200 - 20, 15, 100);
+ctx.fillRect(350 - 40, 200 - 20, 15, 100);
+ctx.fillRect(260 - 40, 200 - 20, 60, 200);
+
+ctx.arc(250, 100, 50, 0, 2*Math.PI);
+ctx.fill();
 
 ctx.beginPath();
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
-ctx.fillStyle = "red";
+ctx.arc(260 + 10, 80, 8, Math.PI, 2*Math.PI);
+ctx.arc(220 + 10, 80, 8, Math.PI, 2*Math.PI);
+ctx.fillStyle = "aqua";
 ctx.fill();
