ef6238dcf0623c081691f327bffa5135dca2a2f0
mumbling-brick <123447936+mumbling-brick@users.noreply.github.com>
2025-10-07 21:28:00 +0900

    First Drawing

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("hello");
+const canvas = document.querySelector("canvas");
+canvas.width = 800;
+canvas.height = 800;
+
+const ctx = canvas.getContext("2d");
+
+ctx.fillRect(50, 50, 100, 200);
