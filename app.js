c6b79b4f2e234b0e6339b88abd67bb77e8258bac
mumbling-brick <123447936+mumbling-brick@users.noreply.github.com>
2025-10-08 22:06:04 +0900

    Painting Lines

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ const canvas = document.querySelector("canvas");
 canvas.width = 800;
 canvas.height = 800;
 
+const colors = [
+    "#ffb8b8",
+    "#ff3838",
+    "#ff9f1a",
+    "#c56cf0",
+    "#32ff7e",
+    "#fff200",
+    "#18dcff",
+    "#7efff5",
+    "#7d5fff",
+];
+
 const ctx = canvas.getContext("2d");
+ctx.lineWidth = 2;
 
-ctx.fillRect(200 - 40, 200 - 20, 15, 100);
-ctx.fillRect(350 - 40, 200 - 20, 15, 100);
-ctx.fillRect(260 - 40, 200 - 20, 60, 200);
+function onClick(event) {
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    const color = colors[Math.floor(Math.random()*colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
 
-ctx.arc(250, 100, 50, 0, 2*Math.PI);
-ctx.fill();
-
-ctx.beginPath();
-ctx.arc(260 + 10, 80, 8, Math.PI, 2*Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2*Math.PI);
-ctx.fillStyle = "aqua";
-ctx.fill();
+canvas.addEventListener("mousemove", onClick);
