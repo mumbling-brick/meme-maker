110698c6e37eb81d8379cc24244c574b2844c1ed
mumbling-brick <123447936+mumbling-brick@users.noreply.github.com>
2025-10-10 23:44:40 +0900

    Filling Mode

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ canvas.height = 800;
 const ctx = canvas.getContext("2d");
 ctx.lineWidth = 5;
 let isPainting = false;
+let isFilling = false;
+
 
 const lineWidth = document.getElementById("line-width");
 function LineWidthSelection(event) {
@@ -16,16 +18,13 @@ function ColorSelection(event) {
     ctx.strokeStyle = event.target.value;
     ctx.fillStyle = event.target.value;
 }
-
 const ColorOptions = Array.from(document.getElementsByClassName("color-option"));
-
 function onColorCLick(event) {
-    const colorValue =event.target.dataset.color; 
+    const colorValue = event.target.dataset.color; 
     ctx.strokeStyle = colorValue;
     ctx.fillStyle = colorValue;
     Color.value = colorValue;
 }
-
 
 
 function drawLining(event) {
@@ -37,21 +36,38 @@ function drawLining(event) {
         ctx.moveTo(event.offsetX, event.offsetY);
     }
 }
-
 function startLining() {
     isPainting = true;
 }
-
 function endLining() {
     isPainting = false;
 }
+
+const modeBtn = document.getElementById("mode-btn");
+function onModeClick() {
+    if(isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+function onCanvasClick() {
+    if(isFilling) {
+        ctx.fillRect(0, 0, 800, 800);
+    }
+}
+
 
 canvas.addEventListener("mousemove", drawLining);
 canvas.addEventListener("mousedown", startLining);
 canvas.addEventListener("mouseup", endLining);
 canvas.addEventListener("mouseleave", endLining);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", LineWidthSelection);
 
 Color.addEventListener("change", ColorSelection);
 ColorOptions.forEach(color => color.addEventListener("click", onColorCLick));
+modeBtn.addEventListener("click", onModeClick);
