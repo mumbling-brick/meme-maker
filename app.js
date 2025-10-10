d007f8c3b76e9af218334f17c2d58ba858d34a21
mumbling-brick <123447936+mumbling-brick@users.noreply.github.com>
2025-10-10 16:09:25 +0900

    Line Width

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ canvas.width = 800;
 canvas.height = 800;
 
 const ctx = canvas.getContext("2d");
-ctx.lineWidth = 2;
+ctx.lineWidth = 5;
 let isPainting = false;
 
-function onMove(event) {
+const lineWidth = document.getElementById("line-width");
+function LineWidthSelection(event) {
+    ctx.lineWidth = event.target.value;
+}
+
+
+function drawLining(event) {
     if(isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY);
         ctx.stroke();
-        return;
     } else {
+        ctx.beginPath();
         ctx.moveTo(event.offsetX, event.offsetY);
     }
-    
 }
 
 function startLining() {
@@ -25,7 +30,10 @@ function endLining() {
     isPainting = false;
 }
 
-canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousemove", drawLining);
 canvas.addEventListener("mousedown", startLining);
 canvas.addEventListener("mouseup", endLining);
 canvas.addEventListener("mouseleave", endLining);
+
+
+lineWidth.addEventListener("change", LineWidthSelection);
